891e43279c77792eaf3948e2655ab23f22ba708c
Vyacheslav <thirtylynx@gmail.com>
2017-08-20 16:21:56 +0500

    Add empty array

---
+++ js/map.js
@@ -0,0 +1,33 @@
+'use strict';
+var AVATARS;
+var renderRandomNumber = function (value) {
+};
+
+var realty = [
+  {
+    author: {
+      avatar: './img/avatars/user{{xx}}.png'
+    },
+    offer: {
+      tittle: '',
+      address: '',
+      price: '',
+      type: '',
+      rooms: '',
+      guests: '',
+      checkin: '',
+      checkout: '',
+      features: '',
+      description: '',
+      photos: ''
+    },
+    location: {
+      x: '',
+      y: ''
+    }
+  }
+];
+
+var NUMBERS = ['01', '02', '03', '04', '05', '06', '07', '08'];
+var randomNumerOld = 0;
+
