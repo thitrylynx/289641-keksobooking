029873bcf88cc14e0ad776a5825f48de7d63018f
Vyacheslav <thirtylynx@gmail.com>
2017-08-21 10:49:17 +0500

    Add random sort and pop function.

--- js/map.js
+++ js/map.js
@@ -1,12 +1,63 @@
 'use strict';
-var AVATARS;
-var renderRandomNumber = function (value) {
+
+var AVATAR_NUMBERS = ['01','02','03', '04', '05', '06', '07', '08'];
+var compareRandom = function(a, b) {
+  return Math.random() - 0.5;
+};
+AVATAR_NUMBERS.sort(compareRandom);
+var popNumber = function () {
+  var poped = AVATAR_NUMBERS.pop();
+  return poped;
 };
 
 var realty = [
   {
     author: {
-      avatar: './img/avatars/user{{xx}}.png'
+      avatar: './img/avatars/user' + popNumber() + '.png'
+    },
+    offer: {
+      tittle: '',
+      address: '',
+      price: '',
+      type: '',
+      rooms: '',
+      guests: '',
+      checkin: '',
+      checkout: '',
+      features: '',
+      description: '',
+      photos: ''
+    },
+    location: {
+      x: '',
+      y: ''
+    }
+  },
+  {
+    author: {
+      avatar: './img/avatars/user' + popNumber() + '.png'
+    },
+    offer: {
+      tittle: '',
+      address: '',
+      price: '',
+      type: '',
+      rooms: '',
+      guests: '',
+      checkin: '',
+      checkout: '',
+      features: '',
+      description: '',
+      photos: ''
+    },
+    location: {
+      x: '',
+      y: ''
+    }
+  },
+  {
+    author: {
+      avatar: './img/avatars/user' + popNumber() + '.png'
     },
     offer: {
       tittle: '',
