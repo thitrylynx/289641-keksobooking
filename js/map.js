1bc543e6ff0d6893db92aeddab1ebd82f8b7e909
Vyacheslav <thirtylynx@gmail.com>
2017-08-27 14:28:48 +0500

    Beautify code. Add functions

--- js/map.js
+++ js/map.js
@@ -42,65 +42,73 @@ var FEATURES = [
 var PIN_WIDTH = 40;
 var PIN_HEIGHT = 40;
 
+// функция случайной сортировки массива
 var compareRandom = function () {
   return Math.random() - 0.5;
 };
+
+// функция получения случайного числа на заданном промежутке
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+// функция получения случайного числа для массива
 var renderRandomNumber = function (value) {
-  var valueResult = Math.floor(Math.random() * (value.length));
-  return valueResult;
+  return Math.floor(Math.random() * (value.length));
 };
-AVATAR_NUMBERS.sort(compareRandom);
-TITLES.sort(compareRandom);
 
-var popNumber = function () {
-  var popedAvatar = AVATAR_NUMBERS.pop();
-  return popedAvatar;
+// функция получения случайного неповторяющегося значения из массива (аватар)
+var getUniqueAvatar = function () {
+  AVATAR_NUMBERS.sort(compareRandom);
+  return AVATAR_NUMBERS.pop();
 };
-var popTitle = function () {
-  var popedTitle = TITLES.pop();
-  return popedTitle;
+
+// функция получения случайного неповторяющегося значения из массива (заголовок)
+var getuUniqueTitle = function () {
+  TITLES.sort(compareRandom);
+  return TITLES.pop();
 };
-var getRandomLength = function () {
-  var randomLengthArr = [];
-  var randomLength = getRandomInt(1, 6);
-  for (var i = 0; i < randomLength; i++) {
-    randomLengthArr[i] = FEATURES[i];
+
+// функция получения случайной длины массива
+var getRandomArrayLength = function (array) {
+  return array.slice(0, getRandomInt(0, array.length));
+};
+
+// функция создания массива случайных объявлений
+var renderRandomDialogsArr = function (array, length) {
+  for (var j = 0; j < length; j++) {
+    var LOCATION_X = getRandomInt(300, 900);
+    var LOCATION_Y = getRandomInt(100, 500);
+    array.push({
+      author: {
+        avatar: './img/avatars/user' + getUniqueAvatar() + '.png'
+      },
+      offer: {
+        title: getuUniqueTitle(),
+        address: LOCATION_X + ', ' + LOCATION_Y,
+        price: getRandomInt(1000, 1000000),
+        type: TYPES[renderRandomNumber(TYPES)],
+        rooms: getRandomInt(1, 5),
+        guests: getRandomInt(1, 10),
+        checkin: TIME[renderRandomNumber(TIME)],
+        checkout: TIME[renderRandomNumber(TIME)],
+        features: getRandomArrayLength(FEATURES),
+        description: '',
+        photos: []
+      },
+      location: {
+        x: LOCATION_X,
+        y: LOCATION_Y
+      }
+    });
   }
-  return randomLengthArr;
+  return array;
 };
-for (var j = 0; j < 8; j++) {
-  var LOCATION_X = getRandomInt(300, 900);
-  var LOCATION_Y = getRandomInt(100, 500);
-  OFFERS.push({
-    author: {
-      avatar: './img/avatars/user' + popNumber() + '.png'
-    },
-    offer: {
-      title: popTitle(),
-      address: LOCATION_X + ', ' + LOCATION_Y,
-      price: getRandomInt(1000, 1000000),
-      type: TYPES[renderRandomNumber(TYPES)],
-      rooms: getRandomInt(1, 5),
-      guests: getRandomInt(1, 10),
-      checkin: TIME[renderRandomNumber(TIME)],
-      checkout: TIME[renderRandomNumber(TIME)],
-      features: getRandomLength(),
-      description: '',
-      photos: ''
-    },
-    location: {
-      x: LOCATION_X,
-      y: LOCATION_Y
-    }
-  });
-}
 
-var pinMap = document.querySelector('.tokyo__pin-map');
-var fragmentPin = document.createDocumentFragment();
-var renderFragmentPinMap = function (object, element) {
+// функция отрисовки маркеров
+var renderFragmentPinMap = function (object) {
+  var pinMap = document.querySelector('.tokyo__pin-map');
+  var fragmentPin = document.createDocumentFragment();
   for (var k = 0; k < 8; k++) {
     var newElement = document.createElement('div');
     var imgElement = document.createElement('img');
@@ -112,23 +120,25 @@ var renderFragmentPinMap = function (object, element) {
     imgElement.style.width = PIN_WIDTH + 'px';
     imgElement.style.height = PIN_HEIGHT + 'px';
     imgElement.setAttribute('src', object[k].author.avatar);
-    element.appendChild(newElement);
+    fragmentPin.appendChild(newElement);
     newElement.appendChild(imgElement);
   }
+  pinMap.appendChild(fragmentPin);
 };
-renderFragmentPinMap(OFFERS, fragmentPin);
-pinMap.appendChild(fragmentPin);
 
-var offerPanel = document.getElementById('offer-dialog');
-var offerPanelTemplate = document.getElementById('lodge-template').content;
+// функция отрисовки объявлений
 var renderOfferPanel = function (offer) {
+  var offerPanelTemplate = document.getElementById('lodge-template').content;
   var panelElement = offerPanelTemplate.cloneNode(true);
   var fragmentFeature = document.createDocumentFragment();
+  var dialogImage = document.querySelector('.dialog__title img');
+
   for (var l = 0; l < offer.features.length; l++) {
     var feature = document.createElement('span');
     feature.className = 'feature__image feature__image--' + offer.features[l];
     fragmentFeature.appendChild(feature);
   }
+
   panelElement.querySelector('.lodge__title').textContent = offer.title;
   panelElement.querySelector('.lodge__address').textContent = offer.address;
   panelElement.querySelector('.lodge__price').innerHTML = offer.price + '&#x20bd;/ночь';
@@ -137,10 +147,17 @@ var renderOfferPanel = function (offer) {
   panelElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + offer.checkin + ', ' + 'выезд до ' + offer.checkout;
   panelElement.querySelector('.lodge__features').appendChild(fragmentFeature);
   panelElement.querySelector('.lodge__description').textContent = offer.description;
+  dialogImage.setAttribute('src', OFFERS[0].author.avatar);
   return panelElement;
 };
+
+renderRandomDialogsArr(OFFERS, 8);
+renderFragmentPinMap(OFFERS);
+
+var offerPanel = document.getElementById('offer-dialog');
 var getOfferPanel = renderOfferPanel(OFFERS[0].offer);
+
 var fragmentPanel = document.createDocumentFragment();
 offerPanel.appendChild(getOfferPanel);
 offerPanel.replaceChild(fragmentPanel, offerPanel.children[1]);
-document.querySelector('.dialog__title img').setAttribute('src', OFFERS[0].author.avatar);
+
